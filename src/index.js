c0e06b43ccbf7b35876ca1e0d8897a2155a6c447
calvin <calvinhaviandy@gmail.com>
2024-10-06 13:12:36 +0700

    renewing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+import { inject } from '@vercel/analytics'; 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+inject();
+
 root.render(
   <React.StrictMode>
     <App />
